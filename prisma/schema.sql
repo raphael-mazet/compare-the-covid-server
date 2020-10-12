864de87e69da36e3be5cfdd5af6890e2fcdda0db
David Hardy <david.hardy50@gmail.com>
2020-10-12 19:48:53 +0200

    refactor postgres db schema

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -2,9 +2,19 @@ CREATE TYPE alert_type AS ENUM ('confirmed', 'suspected', 'safe');
 
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "username" varchar,
-  "password" varchar,
+  "username" varchar UNIQUE,
+  "password" varchar UNIQUE,
   "email" varchar
+);
+
+CREATE TABLE "locations" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "country" varchar,
+  "googlemap_URL" varchar UNIQUE,
+  "location_type" varchar,
+  "longitude" float,
+  "latitutde" float
 );
 
 CREATE TABLE "events" (
@@ -12,20 +22,10 @@ CREATE TABLE "events" (
   "alertType" alert_type,
   "alertDate" date,
   "alertScore" int,
+  "location_id" int,
+  FOREIGN KEY ("location_id") REFERENCES "locations"("id"),
   "created_at" timestamp,
   "expires_on" timestamp
-);
-
-CREATE TABLE "locations" (
-  "id" SERIAL PRIMARY KEY,
-  "location_events" int,
-  FOREIGN KEY ("location_events") REFERENCES "events"("id") ON DELETE CASCADE,
-  "name" varchar,
-  "country" varchar,
-  "googlemap_URL" varchar,
-  "location_type" varchar,
-  "longitude" float,
-  "latitutde" float
 );
 
 CREATE TABLE "savedLocations" (
